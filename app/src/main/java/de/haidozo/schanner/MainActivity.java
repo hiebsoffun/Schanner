84906361d3b01937ddba545609a9f48be2c663de
Michael Hiebl <michihiebl@web.de>
2015-01-08 20:37:04 +0100

    added image intent

--- app/src/main/java/de/haidozo/schanner/MainActivity.java
+++ app/src/main/java/de/haidozo/schanner/MainActivity.java
@@ -1,21 +1,38 @@
 package de.haidozo.schanner;
 
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
 import android.provider.MediaStore;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import org.opencv.android.OpenCVLoader;
 
-public class MainActivity extends ActionBarActivity {
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class MainActivity extends Activity {
 
     private static final String     TAG                                 = "Schanner::MainActivity";
     private static final int        CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-    private Uri                     fileUri;
+    private Uri outputFileUri;
+    @InjectView(R.id.scan_button) Button button;
 
     // Native methods
     private native String hello();
@@ -29,27 +46,56 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        ButterKnife.inject(this);
 
-        //fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
-        //intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.openImageIntent();
+            }
+        });
+    }
 
-        // start the image capture Intent
-        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    private void openImageIntent() {
+        // Determine Uri of camera image to save.
+        final File root = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + File.separator + "Schanner" + File.separator);
+        root.mkdirs();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+        Date now = new Date();
+        String fileName = formatter.format(now) + ".jpg";
+        final File sdImageMainDirectory = new File(root, fileName);
+        outputFileUri = Uri.fromFile(sdImageMainDirectory);
+
+        // Camera.
+        final List<Intent> cameraIntents = new ArrayList<Intent>();
+        final Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        final PackageManager packageManager = getPackageManager();
+        final List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
+        for(ResolveInfo res : listCam) {
+            final String packageName = res.activityInfo.packageName;
+            final Intent intent = new Intent(captureIntent);
+            intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            intent.setPackage(packageName);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+            cameraIntents.add(intent);
+        }
+
+        // Filesystem.
+        final Intent galleryIntent = new Intent();
+        galleryIntent.setType("image/*");
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+        // Chooser of filesystem options.
+        final Intent chooserIntent = Intent.createChooser(galleryIntent, "Take image from...");
+
+        // Add the camera options.
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray(new Parcelable[]{}));
+
+        startActivityForResult(chooserIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (!OpenCVLoader.initDebug()) {
-            Log.e(TAG, "OpenCVLoader.initDebug() failed.");
-        } else {
-            Log.d(TAG, "OpenCV library found inside package. Using it!");
-            // Load native library after(!) OpenCV initialization
-            System.loadLibrary("schafkopf_score_scanner");
-        }
-    }
+    protected void onResume() { super.onResume(); }
 
     @Override
     protected void onPause() {
@@ -59,6 +105,13 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(TAG, "OpenCVLoader.initDebug() failed.");
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            // Load native library after(!) OpenCV initialization
+            System.loadLibrary("schafkopf_score_scanner");
+        }
     }
 
     @Override
